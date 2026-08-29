831780d1d7f1e6a0004f31946532186ad00ce1ad
naveenkumarkummara6-creator <naveenkumarkummara6@gmail.com>
2026-08-29 11:32:53 +0530

    Time: 7 ms (41.55%), Space: 19.3 MB (52.54%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num <= 1:
+            return False
+        total = 1
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                total += i
+                if i * i != num:
+                    total += num // i
+            i += 1
+            
+        return total == num
